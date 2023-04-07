d44a128366e05bb313baa529d82a7c5a64849811
Malachi <malachi.ch1695@gmail.com>
2023-04-07 16:42:44 +1000

    Update CalcSine.asm
    Older versions of SCE featured an optimized version of CalcSine, which got reverted likely due to being created by a now controversial person, and generally not being a massive game changing speedup.
    Due to a bit of guilt I have around that code, and a new philosophy I've developed for situations like this, I decided to rewrite and outclass that original code, effectively obsoleting it.
    My version is equal in speed to that version if you use it normally, but allows you to dance with unsafe register use, if your code clears the high byte of d1.
    I also included a WIP macro version, which uses an aX register of your choice (I tested with a1). That versions code is slower, but is easy to inline which overall saves cycles.

--- Data/Objects/CalcSine.asm
+++ Data/Objects/CalcSine.asm
@@ -7,14 +7,63 @@
 
 CalcSine:
 GetSineCosine:
-		andi.w	#$FF,d0
-		add.w	d0,d0
-		addi.w	#$40*2,d0	; $40 = 90 degrees, sin(x+90) = cos(x)
-		move.w	SineTable(pc,d0.w),d1	; cos
-		subi.w	#$40*2,d0
-		move.w	SineTable(pc,d0.w),d0	; sin
+; the two before the uncommented addq are basically a makeshift andi.w #$FF,d0, but moving d0 to d1
+; this allows cosine to be handled last, and opens an optional unsafe call which can save 4 cycles
+		clr.w	d1
+CalcSine.unsafe:
+GetSineCosine.unsafe:
+		move.b	d0,d1
+;		addq.w	#8/2,d1				; add 8 (4x2) so cosine can reach further
+		add.w	d1,d1				; double because we're handling words
+		move.w	SineTable(pc,d1.w),d0		; sin
+		addq.w	#8,d1				; add 8 so cosine can reach further
+		move.w	SineTable+($40*2)-8(pc,d1.w),d1	; cos ; $40 = 90 degrees, sin(x+90) = cos(x)
 		rts
 ; ---------------------------------------------------------------------------
 
 SineTable:	binclude "Misc Data/Sine.bin"
 	even
+; ---------------------------------------------------------------------------
+; if calc = 0 or nothing, get sine in d0 and cosine in d1
+; if 1, get   sine in d0
+; if 2, get cosine in d1
+; if 3, get cosine in d0
+calcsine_macro macro reg,calc,unsafe
+	if ("calc"="0") || ("calc"="")
+	    if ("unsafe"="0") || ("unsafe"="")
+		clr.w	d1
+	    endif
+		move.b	d0,d1
+		add.w	d1,d1				; double because we're handling words
+		lea	(SineTable).w,reg
+		add.w	d1,reg				; add angle to sine table
+		move.w	(reg),d0			; sin
+		move.w	$40*2(reg),d1			; cos ; $40 = 90 degrees, sin(x+90) = cos(x)
+	elseif calc=1
+		andi.w	#$FF,d0
+		add.w	d0,d0				; double because we're handling words
+		lea	(SineTable).w,reg
+		move.w	(reg,d0.w),d0			; sin
+	elseif calc=2
+	    if ("unsafe"="0") || ("unsafe"="")
+		clr.w	d1
+	    endif
+		move.b	d0,d1
+		add.w	d1,d1				; double because we're handling words
+		lea	(SineTable+($40*2)).w,reg	; $40 = 90 degrees, sin(x+90) = cos(x)
+		move.w	(reg,d1.w),d1			; cos
+	elseif calc=3
+		andi.w	#$FF,d0
+		add.w	d0,d0				; double because we're handling words
+		lea	(SineTable+($40*2)).w,reg	; $40 = 90 degrees, sin(x+90) = cos(x)
+		move.w	(reg,d0.w),d0			; cos
+	else
+	fatal "CalcSine macro type not defined"
+	endif
+	endm
+
+;	calcsine_macro a1
+;	calcsine_macro a1,0
+;	calcsine_macro a1,1
+;	calcsine_macro a1,2
+;	calcsine_macro a1,3
